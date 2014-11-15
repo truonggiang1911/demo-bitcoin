7d46a42fcfe577a877ed8e3fc5fb61d85f361aec
truonggiang1911 <truonggiang19112000@yahoo.com>
2014-11-15 14:27:31 +0700

    add filter secret for callback - and get all Money Currency for form order

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -1,12 +1,19 @@
 class PaysController < ApplicationController
-  
-  
+
+	#config SECRET_TOKEN_APP for 3 environment
+    SECRET_TOKEN_APP = "g8u4f4834grghu9hteruhe0ugldr88"
+	before_filter :check_secret_token_callback, :only => [:pay_callback, :pay_cancel]
 	def order_payment
 		@document = Document.find_by_id(params[:document_id])
 	end	
 
 	 def pay_with_bitcoin
 	 	if (params[:document].present?)
+	 		currency = params[:document][:currency].present? ? params[:document][:currency] : "BTC"
+			amount_total = params[:document][:price].present? ? params[:document][:price].to_f : 0.0
+			user_id = current_user.id
+			document_id = params[:document][:id]
+
 	 		user_credentials = {
 		    	:access_token => 'access_token',
 		    	:refresh_token => 'refresh_token',
@@ -17,26 +24,24 @@ class PaysController < ApplicationController
 	 				  	{
 	 					 	name: "test",
 	 						type: "buy_now", 
-	 						callback_url: "http://localhost:3000/pay_callback?secret=834u9hteruhgrghe0ugldrg8u4f487",
-	 						# success_url: "https://localhost:3000/pay_successful",
-	 						# cancel_url: "https://localhost:3000/pay_cancel",
-	 						# auto_redirect: true,
-	 						# auto_redirect_success: true,
-	 						# auto_redirect_cancel: true,
+	 						custom: document_id,
+	 						callback_url: "http://localhost:3000/pay_callback?secret=#{SECRET_TOKEN_APP}",
+	 						success_url: "http://localhost:3000/pay_successful?secret=#{SECRET_TOKEN_APP}",
+	 						cancel_url: "http://localhost:3000/pay_cancel?secret=#{SECRET_TOKEN_APP}",
+	 						auto_redirect: true,
+	 						auto_redirect_success: true,
+	 						auto_redirect_cancel: true,
 	 						description: "Sample description",
 	 						style: "custom_large"
 	 						}
 	 					}
-			currency = params[:document][:currency].present? ? params[:document][:currency] : "BTC"
-			amount_total = params[:document][:price].present? ? params[:document][:price].to_f : 0.0
-			user_id = current_user.id
-			document_id = params[:document][:id]
+
 			r = $coinbase.create_button "Your Payment redirect_to", amount_total.to_money(currency), "Testing with #{amount_total}#{currency}", "my custom tracking code for this order", options
 			puts r.inspect
 			puts r.button.code
 			puts r.button.callback_url
 			puts r.button.success_url
-	  		redirect_to "https://coinbase.com/checkouts/#{r.button.code}?user_id=#{user_id}&document_id=#{document_id}"
+	  		redirect_to "https://coinbase.com/checkouts/#{r.button.code}"
 
 	 	else
 	 		redirect_to "/"
@@ -55,9 +60,10 @@ class PaysController < ApplicationController
 	 	 # 	signer_id: integer, stripe_card_token: string, stripe_customer_id: string)
 	 	order = params[:order]
 	 	puts 22222
+	 	puts order.inspect
 	 	if (order.present? and order[:status] != "expired")
 	 		payment = Payment.new({
- 				document_id: document_id,
+ 				document_id: order[:custom],
 	 			signer_id: current_user.id,
 	 			payment_amount: order[:total_btc][:cents],
 	 			bitcoin_transaction_id: order[:transaction][:id],
@@ -70,7 +76,6 @@ class PaysController < ApplicationController
 	 	else
 	 		redirect_to "/pays/pay_not_successful"
 	 	end	
-
 	 end	
 
 	 def pay_not_successful
@@ -84,4 +89,14 @@ class PaysController < ApplicationController
 	 def pay_cancel
 	 	puts params.inpsect
 	 end	
+
+
+	 private
+	 	#check scret token app to valid callback
+	 	def check_secret_token_callback
+	 		if (params[:secret] == SECRET_TOKEN_APP)
+	 		else
+	 			redirect_to "/"
+	 		end	
+	 	end	
 end
