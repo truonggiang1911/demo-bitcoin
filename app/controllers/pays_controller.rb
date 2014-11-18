2a10e3155eb4f5cc9d61e8d30621e7e19a5f296b
truonggiang1911 <truonggiang19112000@yahoo.com>
2014-11-18 18:22:08 +0700

    remove comment code

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -16,18 +16,6 @@ class PaysController < ApplicationController
 			user_id = current_user.id
 			document_id = params[:document][:id]
 
-	 		user_credentials = {
-		    	:access_token => 'access_token',
-		    	:refresh_token => 'refresh_token',
-		    	:expires_at => Time.now + 1.day
-			}
-
-			# need to Set up callback_url, success_url, cancel_url for 3 enviroments
-			# and get them by EVN[..]
-
-			# if want to config options for button. Please flow the this link to see more details
-			# https://www.coinbase.com/api/doc/1.0/buttons/create.html
-
 	 		options = { button: 
 	 				  	{
 	 					 	name: "test",
@@ -45,10 +33,6 @@ class PaysController < ApplicationController
 	 					}
 
 			r = $coinbase.create_button "Your Payment redirect_to", amount_total.to_money(currency), "Testing with #{amount_total}#{currency}", "my custom tracking code for this order", options
-			puts r.inspect
-			puts r.button.code
-			puts r.button.callback_url
-			puts r.button.success_url
 	  		redirect_to "https://coinbase.com/checkouts/#{r.button.code}"
 
 	 	else
@@ -57,23 +41,7 @@ class PaysController < ApplicationController
 	 end
 
 	 def pay_callback
-	 	document_id = params[:document_id]
-	 	user_id = params[:user_id]
-
-	 	#check account payment
-	 	 # Payment(id: integer, document_id: integer, 
-	 	 # 	is_payment_with_signature: boolean, 
-	 	 # 	payment_amount: float, created_at: 
-	 	 # 	datetime, updated_at: datetime, 
-	 	 # 	signer_id: integer, stripe_card_token: string, stripe_customer_id: string)
 	 	order = params[:order]
-	 	puts 22222
-	 	puts order.inspect
-
-        # this code below just check for case Order Callback Example
-	 	# May be you need to recheck one or more case for callback params
-	 	# Ex: Payout Callback Example
-	 	# Please see more detail https://www.coinbase.com/docs/merchant_tools/callbacks	
 	 	if (order.present? and order[:status] != "expired")
 	 		payment = Payment.new({
  				document_id: order[:custom],
@@ -104,10 +72,6 @@ class PaysController < ApplicationController
 
 	 def pay_cancel
 
-	 	puts "222 go to pay cancel"
-	 	puts params.inspect
-	 	# return to page to notify cancel payment
-	 	# or page for reorder
 	 	order = params[:order]
 	 	if (order.present?)
 	 		redirect_to "/order_payment?document_id=#{order[:custom]}"
@@ -118,11 +82,6 @@ class PaysController < ApplicationController
 
 
 	 def pay_successful
-	 	puts "222 go to pay successful"
-	 	puts params.inspect
-
-	 	#bitcoin_total_btc:float bitcoin_total_native:float bitcoin_total_payout:float
-
 	 	order = params[:order]
 
 	 	if (order.present? and order[:status] != "expired")
